1b74cf4861337a231f8951ca1e0b0366dfc82c13
Megan <mel56@duke.edu>
2019-04-19 23:34:49 -0400

    updated vga controller

--- vga_controller.v
+++ vga_controller.v
@@ -67,6 +67,20 @@ wire[23:0] yahtzee_name;
 
 logo_data logod(.address(yahtzee_ctr), .clock( VGA_CLK_n), .q(index));
 logo_index logoi(.address(index), .clock(iVGA_CLK), .q(yahtzee_name));
+
+wire[7:0] startButtonIndex;
+reg[18:0] startButtonCtr;
+wire[23:0] startButtonBGR;
+
+startbutton_data sbdata(.address(startButtonCtr), .clock(VGA_CLK_n), .q(startButtonIndex));
+startbutton_index sbindex(.address(startButtonIndex), .clock(iVGA_CLK), .q(startButtonBGR));
+
+wire[7:0] playerButtonIndex;
+reg[18:0] playerButtonCtr;
+wire[23:0] playerButtonBGR;
+
+playerbutton_data pbdata(.address(playerButtonCtr), .clock(VGA_CLK_n), .q(playerButtonIndex));
+playerbutton_index pbindex(.address(playerButtonIndex), .clock(iVGA_CLK), .q(playerButtonBGR));
 	
 	
 /********** Determine row, column of screen that address points to *******/
@@ -78,6 +92,8 @@ begin
 	x <= 19'd0;
 	y <= 19'd0;
 	yahtzee_ctr <= 19'd0;
+	startButtonCtr <= 19'd0;
+	playerButtonCtr <= 19'd0;
 end
 	
 /******* MIF Data toggle *********/
@@ -102,16 +118,41 @@ input[31:0] mif_toggle; // Toggles to various data when various sprites should a
 //////
 //////latch valid data at falling edge;
 always@(posedge VGA_CLK_n) begin 
+if (ADDR == 0) begin
+	yahtzee_ctr = 0;
+	startButtonCtr = 0;
+	playerButtonCtr = 0;
+end
+if (mif_toggle == 32'b0) begin
 if ((x>=63) && (x<559) && (y>=81) && (y<186)) begin
 			yahtzee_ctr = yahtzee_ctr + 1;
-			if (yahtzee_ctr >= 52080) begin
-				yahtzee_ctr = 0;
-			end
+//			if (yahtzee_ctr >= 52080) begin
+//				yahtzee_ctr = 0;
+//			end
 //			bgr_data_raw <= 24'h120d20;
 			bgr_data_raw <= yahtzee_name;
+		end else if ((x>= 228) && (x<400) && (y>=204) && (y<372)) begin
+			startButtonCtr = startButtonCtr + 1;
+			bgr_data_raw <= startButtonBGR;
 		end else begin
 			bgr_data_raw <= 24'h150088;
 		end
+end
+if (mif_toggle == 32'd1) begin
+	if ((x>=63) && (x<559) && (y>=81) && (y<186)) begin
+			yahtzee_ctr = yahtzee_ctr + 1;
+//			if (yahtzee_ctr >= 52080) begin
+//				yahtzee_ctr = 0;
+//			end
+//			bgr_data_raw <= 24'h120d20;
+			bgr_data_raw <= yahtzee_name;
+		end else if ((x>=230) && (x<399) && (y>=205) && (y<461)) begin
+			playerButtonCtr = playerButtonCtr + 1;
+			bgr_data_raw <= playerButtonBGR;
+	end else begin
+		bgr_data_raw <= 24'h150088;
+	end
+end
 bgr_data <= bgr_data_raw;
 end
 
